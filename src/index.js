3c1f35cc25cb05776e9b94820d0ac4ff2fd87e97
unknown <dabahdeshubham83@gmail.com>
2022-12-24 02:16:49 +0530

    understanding immutabilty in updating objects

--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
-import { compose, pipe} from "lodash/fp";
-
-let input="    Javascript    ";
-
-const trim = (str)=>{return str.trim()};
-
-//here we implemented the concept of currying where the parameters are seperated by "( )" instead of ","
-const wrap = type=> str=> `<${type}>${str}</${type}>` ;
-//the motive is to pass the parameter one at a time, so we create a function accepting 1st parameter then make it return a function which accepts the 2nd parameter and so and in the end use all those parameters
-
-const toLowerCase= (str)=>{return str.toLowerCase()};
+/***implemeting immutability in updating objects and arrays
+immutability:an object whose state cannot be modified after it is created. This is in contrast to a mutable object (changeable object), which can be modified after it is created
+example: strings in javascript are immutable means 
+let str1= "shubham";  here the original string does not change and the changes that need to be applied are applied on its copy↓
+let str2= str1.upperCase();***/
 
 
-const pipedFunction= pipe(trim,toLowerCase,wrap("span"));
-const resultPipe=pipedFunction(input);
-console.log(resultPipe);
+
+///******** Understanding immutabilty in Objects *********///
+
+//similarly to create objects immutable in javascript we have to apply changes to its copy here we need to understand that using const keyword for an object only allows it to not be assigned to another object but we can change change the object properties
+const obj1={property1:"value1",property2:"value2"};
+obj1.property1="someOtherValue";
+
+///1 we can use ... operator
+const obj2={...obj1,property1:"someNewValue"}; //here we first assign all the properties of obj1 to obj2 and provide the new value for property1 which overwrites the original value
+
+///2 but the above is just a shallow copy for some nested objects we have to do a deep copy
+const obj3={
+    name:"Ajay",
+    address:{
+        country:"India",
+        city:"mumbai"
+    }
+};
+
+///*here we have to do deep copy*///
+const obj4={...obj3,address:{...obj3.address,city:"Delhi"}}; //here we need to use the spread operator for the address property too because we have to understand that the original and the copy share the same property "address" in the memory so if we change the address without creating its copy the address in the original object will change too
+
+
+
 
 
 
